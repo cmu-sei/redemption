ba131f6d33716583ce2d00faab4c733cb729158a
Joseph Christopher Sible <jcsible@cert.org>
2024-04-01 11:12:54 -0400

    Pull request #68: Add examples of some of the kinds of findings from CodeChecker
    Merge in REM/redemption.public from codechecker-examples to main
    
    Squashed commit of the following:
    
    commit 8b0833c7d8739aefe5c9b53af761fa0ae613d9b7
    Author: Joseph C. Sible <jcsible@cert.org>
    Date:   Thu Mar 28 10:55:15 2024 -0400
    
        Add legal tags to everything
    
    commit 099bc6bccc78f611c763a8a87078c3a8dbb047a3
    Author: Joseph C. Sible <jcsible@cert.org>
    Date:   Wed Mar 27 13:51:02 2024 -0400
    
        Add examples of some of the kinds of findings from CodeChecker

---
+++ data/codechecker/false/afterfail.cpp
@@ -0,0 +1,48 @@
+// <legal>
+// 'Redemption' Automated Code Repair Tool
+//
+// Copyright 2023, 2024 Carnegie Mellon University.
+//
+// NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING
+// INSTITUTE MATERIAL IS FURNISHED ON AN 'AS-IS' BASIS. CARNEGIE MELLON
+// UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+// AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR
+// PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF
+// THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY
+// KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT
+// INFRINGEMENT.
+//
+// Licensed under a MIT (SEI)-style license, please see License.txt or
+// contact permission@sei.cmu.edu for full terms.
+//
+// [DISTRIBUTION STATEMENT A] This material has been approved for public
+// release and unlimited distribution.  Please see Copyright notice for
+// non-US Government use and distribution.
+//
+// This Software includes and/or makes use of Third-Party Software each
+// subject to its own license.
+//
+// DM23-2165
+// </legal>
+
+#include <cstdlib>
+#include <gtest/gtest.h>
+
+TEST(Foo, Bar) {
+  int *x = static_cast<int *>(malloc(sizeof *x));
+  if(!x) {
+    FAIL();
+  }
+  *x = 2;
+  *x += 2;
+  if(*x != 4) {
+    free(x);
+    FAIL();
+  }
+  *x -= 5;
+  if(*x != -1) {
+    free(x);
+    FAIL();
+  }
+  free(x);
+}
