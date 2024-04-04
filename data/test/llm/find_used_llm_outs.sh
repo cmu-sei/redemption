43b7b52368cde1d2a9771cf3c025b462f8babd1a
Joseph Christopher Sible <jcsible@cert.org>
2024-04-04 13:10:51 -0400

    Pull request #73: Follow-up to REM-19(b): simplify my jq and add hypothetical rationales
    Merge in REM/redemption.public from rationale-and-simplify to main
    
    Squashed commit of the following:
    
    commit c970bc6f6c7a99afc3e76746f4c9ced73926c18e
    Author: Joseph C. Sible <jcsible@cert.org>
    Date:   Thu Apr 4 09:48:43 2024 -0400
    
        Add my hypothetical "if-this-code-wasnt-dead" rationales
    
    commit 1fbd13438962b4663b4a5ce51cc5e20140654b63
    Author: Joseph C. Sible <jcsible@cert.org>
    Date:   Thu Apr 4 09:40:44 2024 -0400
    
        Slightly simplify the jq

--- data/test/llm/find_used_llm_outs.sh
+++ data/test/llm/find_used_llm_outs.sh
@@ -1,1 +1,1 @@
-jq -r '.[] | .llm_output_file // empty | select(test("^[0-9a-f]{24}[.]txt$"))' ../*.alerts.json
+jq -r '.[].llm_output_file // empty | select(test("^[0-9a-f]{24}[.]txt$"))' ../*.alerts.json
